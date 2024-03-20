02a21423e4cb001d6bcaa85b090113a22e10dbf2
AyaAl-wahidi <ayanalwahidi@gmail.com>
2024-03-20 14:39:42 +0300

    alert

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 var promptNameResult = prompt("Enter your name")
+if (promptNameResult) {
 
-var promptGenderResult = prompt("Enter your gender").toLocaleLowerCase();
+    var promptGenderResult = prompt("Enter your gender").toLocaleLowerCase();
 
-if (promptGenderResult != "male" && promptGenderResult != "female") {
-    alert("Please enter valid gender");
-} else {
+    if (promptGenderResult != "male" && promptGenderResult != "female") {
+        alert("Please enter valid gender");
+        promptGenderResult = "none";
+    }
+
     var promptAgeResult = prompt("Enter your age")
     if (promptAgeResult <= 0) {
-        alert("Enter valid age!");
+        alert("Not valid!");
     } else {
         var skipMsg = confirm("Do you want to skip the welcoming message?");
         let welcome;
@@ -16,8 +19,11 @@ if (promptGenderResult != "male" && promptGenderResult != "female") {
                 welcome = "Welcome Mr. " + promptNameResult + "!";
             } else if (promptGenderResult === "female") {
                 welcome = "Welcome Ms. " + promptNameResult + "!";
+            } else if (promptGenderResult === "none") {
+                welcome = "Welcome " + promptNameResult + "!";
             }
             alert(welcome);
         }
     }
+} else {
 }
