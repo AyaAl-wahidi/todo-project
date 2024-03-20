1c8d712a346faa6da2ac72cff5ab9adda05a6979
AyaAl-wahidi <ayanalwahidi@gmail.com>
2024-03-20 11:50:30 +0300

    edit alert bot

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
 var promptNameResult = prompt("Enter your name")
 
-var promptGenderResult = prompt("Enter your gender")
+var promptGenderResult = prompt("Enter your gender").toLocaleLowerCase();
 
-var promptAgeResult = prompt("Enter your age")
-if (promptAgeResult <= 0) {
-    var promptAgeResult = prompt("Enter valid age");
-} else if (isNaN(promptAgeResult)) {
-    var promptAgeResult = prompt("Enter a valid age");
-}
-
-switch (promptGenderResult) {
-    case "male":
-        var confirmResult = confirm("Mr. " + promptNameResult);
-        break;
-    case "female":
-        var confirmResult = confirm("Ms. " + promptNameResult);
-        break;
-    default:
-        // alert("Please enter valid gender");
-        // var promptGenderResult = prompt("Enter your gender");
-        var confirmResult = confirm("");
-        break;
+if (promptGenderResult != "male" && promptGenderResult != "female") {
+    alert("Please enter valid gender");
+} else {
+    var promptAgeResult = prompt("Enter your age")
+    if (promptAgeResult <= 0) {
+        alert("Enter valid age!");
+    } else {
+        var skipMsg = confirm("Do you want to skip the welcoming message?");
+        let welcome;
+        if (!skipMsg) {
+            if (promptGenderResult === "male") {
+                welcome = "Welcome Mr. " + promptNameResult + "!";
+            } else if (promptGenderResult === "female") {
+                welcome = "Welcome Ms. " + promptNameResult + "!";
+            }
+            alert(welcome);
+        }
+    }
 }
